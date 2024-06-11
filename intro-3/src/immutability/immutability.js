896951da601971212a231765ba04cdde7a198dd6
cowuake <riccardo_mura@yahoo.com>
2024-06-11 17:23:53 +0200

    Add solution for 'changeUserAddress'

--- intro-3/src/immutability/immutability.js
+++ intro-3/src/immutability/immutability.js
@@ -27,23 +27,19 @@ export const users = Object.freeze([user10])
 // addressChanges è un oggetto che contiene una o più proprietà di Address da cambiare, ad esempio { city: London }
 // Ritornare l'array di utenti con le proprietà cambiate, mantenendo invariate quelle non presenti in addressChanges
 export const changeUsersAddress = (users, addressChanges) => {
-  // const result = []
-  // for (const user of users) {
-  //   const newUser = Object.entries(user).reduce((acc, [key, value]) => {
-  //     if (typeof value === 'object') {
-  //       acc[key] = changeUsersAddress(value, addressChanges)[0]
-  //     } else {
-  //       if (key in Object.keys(addressChanges)) {
-  //         acc[key] = addressChanges[key]
-  //       } else {
-  //         acc[key] = value
-  //       }
-  //     }
-  //     return acc
-  //   }, {})
-  //   result.push(newUser)
-  // }
-  // return result
+  let result = []
+
+  for (const user of users) {
+    const newUser = Object.fromEntries(Object.entries(user))
+    const address = Object.fromEntries(Object.entries(user.address))
+    for (const [key, value] of Object.entries(addressChanges)) {
+      address[key] = value
+    }
+    newUser['address'] = address
+    result.push(newUser)
+  }
+
+  return result
 }
 
 // Ritornare l'array di utenti senza geo in address
